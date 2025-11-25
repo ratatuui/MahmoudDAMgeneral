f41fe1d645c9ca8cdd04315c39686c7fb2d2ba30
A712\dam1 <mcheickmoussa@gmail.com>
2025-11-25 17:20:21 +0100

    pactica 3 revisar

--- MahmoudCM/MahmoudCM/src/main/java/org/example/Practica3_ud3_ZXSpectrum.java
+++ MahmoudCM/MahmoudCM/src/main/java/org/example/Practica3_ud3_ZXSpectrum.java
@@ -15,13 +15,19 @@ public class Practica3_ud3_ZXSpectrum {
         System.out.println("*********Validador de compatibilidad ZX Spectrum*********");
         System.out.println("Introduce la resolucion de tu pantalla (ancho X alto)");
         System.out.println("Ancho:");
-        ancho = teclado.nextInt();
+
+            ancho = teclado.nextInt();
+
+            System.out.println();
+
+
 
         if (!(ancho % 8 == 0)){
             System.out.println("tiene que ser un numero divisible entre 8");
             return;
         }
 
+        teclado.nextLine();
         System.out.println("Alto:");
 
         alto = teclado.nextInt();
@@ -31,27 +37,54 @@ public class Practica3_ud3_ZXSpectrum {
         }
         String matriz[][]=new String[alto][ancho];
         System.out.println("Introduce (linea a linea) los colores de tu imagen para cada pixel");
-        externo:
-        for (int i = 0; i < matriz.length; i++) {
-            String[] fila;
-            fila = teclado.next().split("");
 
-            for (int j = 0; j < matriz[i].length; j++) {
-                matriz[i][j] = fila[j];
-                if (!(fila[j].matches("A-O"))){
-                    System.out.println();
-                }else{
-                    System.out.println("solo se permite letas entre (A-O)");
-                    i--;
-                    continue externo;
-                }
+        for (int i = 0; i < alto; i++) {
+           String fila = teclado.nextLine();
 
-            }
+           if (fila.length() == ancho){
+               for (int j = 0; j < matriz.length; j++) {
+
+                   matriz[i][j] = fila[j];
+               }
+
+               if (fila.matches("[A-O]+")){
+                   matriz[i] = fila.split("");
+               }else {
+                   System.out.println("A-O");
+                   i--;
+               }
+           }
 
         }
-        for (String fila[] :matriz){
+        for (String[] fila: matriz){
             System.out.println(Arrays.toString(fila));
         }
+
+
+
+
+
+//        externo:
+//        for (int i = 0; i < matriz.length; i++) {
+//            String[] fila;
+//            fila = teclado.next().split("");
+//
+//            for (int j = 0; j < matriz[i].length; j++) {
+//                matriz[i][j] = fila[j];
+//                if (!(fila[j].matches("A-O"))){
+//                    System.out.println();
+//                }else{
+//                    System.out.println("solo se permite letas entre (A-O)");
+//                    i--;
+//                    continue externo;
+//                }
+//
+//            }
+//
+//        }
+//        for (String fila[] :matriz){
+//            System.out.println(Arrays.toString(fila));
+//        }
 
 
     }
